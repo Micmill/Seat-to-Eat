59aecbef63876067bf3344c331f1bbf7cd41f040
Micmill <mictsumbu2704@gmail.com>
2022-05-26 19:52:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Link css page -->
+    <link rel="stylesheet" type="text/css" href="">
+    <!-- Footawesome icons link  -->
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>BookToEat</title>
+</head>
+<body>
+   <div class="container">
+    <div class="row">
+        <div class="col text-start">
+            <div class="title">Seat To Eat</div>
+        </div>
+        <div class="col text-end">
+            <button type="button" class="btn btn-secondary">s'inscrire</button>
+        </div>
+    </div>
+    <p><h4><strong> Vos plats préférés ,directement à votre porte.<h4></strong></p>
+    <fieldset>
+         <p><input type="text" name= "pseudo"placeholder="Saissisez votre adresse"></p>
+         <p>Livrer maintenant: 
+             <select name="Livrer maintenant">
+                <option>A domicile</option>
+                <option>Point relais</option>
+         </select>
+         </p>
+         <p><input type="text" name="pseudo"placeholder="Choississez vos plats"></p>
+         <div class="col">
+             <button type="button" class="btn btn-secondary">se connecter</button>
+         </div>
+    </fieldset>
+    <p><h5><strong>Retrouvez encore plus de  fonctionnalités dans l'application.</strong></h5></p>
+    <input type="button" value="Télécharger l'application" >
+    <p>Notre adresse Ennasr 2 ref monoprix</p>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+
+
+</body>
+</html>
