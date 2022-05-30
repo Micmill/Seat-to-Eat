cd82c09b702c29cb9d3542da40d38971232b528a
elietbg9 <90126350+elietbg9@users.noreply.github.com>
2022-05-30 17:48:04 +0300

    Update and rename index3.php to signup.php

---
+++ signup.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Link css page -->
+    <link rel="stylesheet" type="text/css" href="">
+    <!-- Footawesome icons link  -->
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sign Up</title>
+
+    <style type="text/css">
+        a{
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-warning">
+        <div class="container-fluid" >
+            <a class="navbar-brand" href="#">Seat To Eat</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+   <div class="container" style="padding-top: 2%">
+   	<p class="text-center fs-1">SIGN UP</p>
+       
+       <div class="row g-3">
+          <div class="col-sm-12">
+              <label for="inputAddress" class="form-label">Nom</label>
+              <input type="text" class="form-control"  placeholder="Tapez votre Nom">
+          </div>
+          <div class="col-sm-12">
+             <label for="inputAddress" class="form-label">Prenom</label>
+             <input type="text" class="form-control"  placeholder="Tapez votre Prenom">
+          </div>
+          <div class="col-sm-12">
+             <label for="inputAddress" class="form-label">Mot de Passe</label>
+             <input type="text" class="form-control"  placeholder="Nouveau mot de passe"> 
+          </div>
+
+          <div class="col-sm-12">
+          	<label for="inputAddress" class="form-label">Confirmez Mot de Passe</label>
+          	<input type="text" class="form-control"  placeholder="Confirmez mot de passe">
+          </div>
+          <div class="col-sm-12 text-center">
+              <button type="button" class="btn btn-secondary">Sign Up</button>
+          </div>
+       </div>
+
+
+   </div>
+
+
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+
+
+</body>
+</html>
